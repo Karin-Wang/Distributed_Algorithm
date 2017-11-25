90af02ec9f8a15956b763d274bee40a1196071b1
Felix Yang <felixyang117+GitHub@hotmail.com>
2017-11-25 10:52:39 +0100

    lab 2

--- src/pericles/CitizenInterface.java
+++ src/pericles/CitizenInterface.java
@@ -1,10 +1,21 @@
 package pericles;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface CitizenInterface extends Remote {
-	public int getID() throws RemoteException;;
-	public String getName() throws RemoteException;;
-	public int getPopularity() throws RemoteException;;
+	public int getID() throws RemoteException;
+	public String getName() throws RemoteException;
+	public int getPopularity() throws RemoteException;
+	public int getPopularityElec() throws RemoteException;
+	public Boolean isPassive() throws RemoteException;
+	public void findNeighbourRemoteHandle() throws MalformedURLException, RemoteException, NotBoundException;
+	public void printNeighbours() throws RemoteException;
+	public void sendPopularityToNext() throws RemoteException;
+	public void recvPopularityFromPrev(int i_pop) throws RemoteException;
+	public void sendMaxToNext() throws RemoteException;
+	public void recvMaxFromPrev(int i_max) throws RemoteException;
+	public void comparePopularityValues() throws RemoteException;
 }
